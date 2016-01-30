d2eaa2fff87dd24a774d136c45a0702b01d2c1d2
Josh Asplund <joshasplund@gmail.com>
2016-01-30 16:01:26 -0700

    Fixed extra newlines in converted csv

--- hdf5/csv_fix.py
+++ hdf5/csv_fix.py
@@ -14,18 +14,18 @@ def main():
     try:
         execname, fn, out_fn = sys.argv
         with open(fn, 'r') as read_fh, open(out_fn, 'w') as out_fh:
-            chunk = 100000  # write chunks at a time, give drive a break
+            chunk = 50000  # write chunks at a time, give drive a break
             lines = []
             line = read_fh.next()
             while not line.strip():
-                lines.append(line)
                 line = read_fh.next()
             lines.append(fix_line(line, True))
-            lines.append(read_fh.next())  # line with simulation time
+            line = read_fh.next()
             for line in read_fh:
-                fixed = fix_line(line)
-                lines.append(fixed)
-                if len(lines) == chunk:
+                if line.strip():
+                    fixed = fix_line(line)
+                    lines.append(fixed)
+                if (len(lines) % chunk) == 0:
                     out_fh.writelines(lines)
                     lines = []
             # write any remaining lines
